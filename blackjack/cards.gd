3a5d586b3d5f2d374893e00f964110e25921ac2d
ganyuke <personalemailforedits@gmail.com>
2025-09-27 20:12:43 -0700

    feat: buggy blackjack implementation (will fix later)

---
+++ blackjack/cards.gd
@@ -0,0 +1,195 @@
+enum Suit { Hearts, Diamonds, Clubs, Spades }
+enum Rank { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+enum HandStatus { Playing, Bust, Blackjack }
+enum RoundOutcome { PlayerWin, DealerWin, Draw, InProgress }
+
+class Card extends RefCounted:
+	var rank: Rank
+	var suit: Suit
+	
+	func _init(s: Suit, r: Rank) -> void:
+		rank = r
+		suit = s
+	
+class Deck extends RefCounted:
+	var cards: Array[Card]
+	
+	func _generate_cards():
+		var deck: Array[Card] = []
+		for s in Suit.values():
+			for r in Rank.values():
+				var card = Card.new(s, r)
+				deck.append(card)		
+		return deck
+	
+	func _init():
+		self.cards = self._generate_cards()
+		
+	func shuffle():
+		self.cards.shuffle()
+		
+	func remaining():
+		return cards
+		
+	func draw():
+		return cards.pop_back()
+
+class Hand extends RefCounted:
+	var cards: Array[Card]
+	
+	func _calc_value(card: Card, count_ace: bool = true):
+		# need to handle whether Ace is 11 or 1 
+		if card.rank == 1 and count_ace:
+			return 11
+		elif card.rank > 9:
+			return 10
+		else:
+			return card.rank + 1
+	
+	func _init():
+		self.cards = []
+		
+	func value():
+		var val: int = 0
+		for card in cards:
+			val += self._calc_value(card)
+		return val
+		
+	func analyze():
+		var val = self.value()
+		print_debug("a hand is being analyzed: " + str(val))
+		
+		if val < 21:
+			return HandStatus.Playing
+		elif val > 21:
+			return HandStatus.Bust
+		else:
+			return HandStatus.Blackjack
+		
+	func push_card(card: Card):
+		cards.append(card)
+		
+#class Player extends RefCounted:
+	#var hand: Hand
+	#
+	#func _init():
+		#self.hand = Hand.new()
+#
+#class User extends Player:
+	#
+#
+#class House extends Player:
+
+class Round extends RefCounted:
+	var deck: Deck
+	var player_hand: Hand
+	var dealer_hand: Hand
+	
+	var playing_hand: Hand
+	var is_player_turn: bool = false
+	var is_game_won: bool = false
+	var turns: int = 0 # need to check natural blackjack
+	
+	func _init() -> void:
+		self.deck = Deck.new()
+		self.deck.shuffle()
+		self.player_hand = Hand.new()
+		self.dealer_hand = Hand.new()
+	
+	func start() -> void:
+		var give_cards = func (hand: Hand) -> void:
+			for i in range(0, 2):
+				var card = self.deck.draw()
+				hand.push_card(card)
+		give_cards.call(player_hand)
+		give_cards.call(dealer_hand)
+		
+		self.playing_hand = self.dealer_hand
+	
+	func eval_action() -> RoundOutcome:
+		# natural blackjack check
+		if turns == 0:
+			var player_val = player_hand.value()
+			var dealer_val = dealer_hand.value()
+			
+			if (player_val == 21 and dealer_val == 21):
+				return RoundOutcome.Draw
+			elif (player_val == 21):
+				return RoundOutcome.PlayerWin
+			elif (dealer_val == 21):
+				return RoundOutcome.DealerWin
+			else:
+				return RoundOutcome.InProgress
+		else:
+			var peek = self.playing_hand.analyze()
+			if (peek == HandStatus.Bust):
+				if (self.is_player_turn):
+					return RoundOutcome.DealerWin
+				else:
+					return RoundOutcome.PlayerWin
+			elif (peek == HandStatus.Blackjack):
+				if (self.is_player_turn):
+					return RoundOutcome.PlayerWin
+				else:
+					return RoundOutcome.DealerWin
+			else:
+				return RoundOutcome.InProgress
+
+	func stand():
+		self.is_player_turn = not self.is_player_turn
+		if (self.is_player_turn):
+			print_debug("the player is now playing")
+			self.playing_hand = self.player_hand
+		else:
+			print_debug("the dealer is now playing")
+			self.playing_hand = self.dealer_hand
+		turns = turns + 1
+		
+	func hit() -> RoundOutcome:
+		var card = self.deck.draw()
+		self.playing_hand.push_card(card)
+		return self.eval_action()
+	
+	# repeat until hit >=17
+	func dealer_play() -> RoundOutcome:
+		var val = self.playing_hand.value()
+		if (val < 17): # <= 17 depending on house rules
+			var outcome = self.hit()
+			print_debug("the dealer is hit for " + str(dealer_hand.value()))
+			return outcome
+		else:
+			print_debug("the dealer is now standing at " + str(val))
+			self.stand()
+			var player_val = player_hand.value()
+			if (val > player_val):
+				return RoundOutcome.DealerWin
+			elif (val < player_val):
+				return RoundOutcome.PlayerWin
+			else:
+				return RoundOutcome.Draw
+
+class Game extends RefCounted:
+	var currentRound: Round
+	
+	func _init():
+		currentRound = Round.new()
+		print_debug("round is starting")
+		
+	func can_player_play():
+		return currentRound.is_player_turn
+	
+	func start():
+		print_debug("game started")
+		self.currentRound.start()
+		var game_status = self.currentRound.eval_action()
+		self.currentRound.stand()
+		return game_status
+		
+	func hit():
+		print_debug("the player hit")
+		return self.currentRound.hit()
+		
+	func stand():
+		print_debug("the player stood")
+		self.currentRound.stand()
+		self.currentRound.dealer_play()
---
+++ blackjack/main_scene.gd
@@ -0,0 +1,68 @@
+extends Node2D
+
+const blackjackGame = preload("res://cards.gd")
+var game = blackjackGame.Game.new()
+
+func update_turn():
+	var turn_text: String
+	if (game.currentRound.is_player_turn):
+		turn_text = "Player turn"
+	else:
+		turn_text = "Dealer turn"
+	$Control/TurnLabel.text = turn_text
+	$Control/DealerHandValue.text = str(game.currentRound.dealer_hand.value())
+	$Control/PlayerHandValue.text = str(game.currentRound.player_hand.value())
+
+func _on_hit_button_pressed() -> void:
+	if (game.can_player_play()):
+		var outcome = game.hit()
+		append_history(win_translator(outcome))
+		update_turn()
+
+func _on_stand_button_pressed() -> void:
+	if (game.can_player_play()):
+		game.stand()
+		
+	var outcome = blackjackGame.RoundOutcome.InProgress
+	while (outcome == blackjackGame.RoundOutcome.InProgress):
+		outcome = game.currentRound.dealer_play()
+		append_history(win_translator(outcome))
+		update_turn()
+
+func append_history(s: String) -> void:
+	$Control/History.text += "\n" + s
+
+func win_translator(outcome: blackjackGame.RoundOutcome) -> String:
+	var historyEntry: String
+	if (outcome == blackjackGame.RoundOutcome.InProgress):
+		return "the game continues"
+	
+	if (outcome == blackjackGame.RoundOutcome.Draw):
+		return "game ended in draw"
+	
+	var was_natural = game.currentRound.turns == 0
+	if (outcome == blackjackGame.RoundOutcome.PlayerWin):
+		var dealer_hand_status = game.currentRound.dealer_hand.analyze()
+		historyEntry = "Player won "
+		if (dealer_hand_status == blackjackGame.HandStatus.Bust):
+			historyEntry += "because Dealer busted."
+		else:
+			if (was_natural):
+				historyEntry += "natural "
+			historyEntry += "blackjack."
+	elif (outcome == blackjackGame.RoundOutcome.DealerWin):
+		var player_hand_status = game.currentRound.player_hand.analyze()
+		historyEntry = "Dealer won "
+		if (player_hand_status == blackjackGame.HandStatus.Bust):
+			historyEntry += "because Player busted."
+		else:
+			if (was_natural):
+				historyEntry += "natural "
+			historyEntry += "blackjack."
+	return historyEntry
+
+func _on_start_button_pressed() -> void:
+	var outcome = game.start()
+	append_history(win_translator(outcome))
+	update_turn()
+	$Control/StartButton.visible = false
